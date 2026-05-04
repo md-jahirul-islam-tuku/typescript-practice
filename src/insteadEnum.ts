72614bc5bc287caa5ece40dce9353c69cd5ccdc7
Tuku <tukuwebian@gmail.com>
2026-05-04 11:57:51 +0300

    insteadEnum learn

---
+++ src/insteadEnum.ts
@@ -0,0 +1,20 @@
+const UserRoles = {
+  Admin: "ADMIN",
+  Editor: "Editor",
+  Viewer: "VIEWER",
+} as const;
+
+// (typeof UserRoles)[keyof typeof UserRoles]) value type হিসেবে ব্যবহার করার জন্য এই পদ্ধতি।
+
+
+const canEdit = (role: (typeof UserRoles)[keyof typeof UserRoles]) => {
+  if (role === UserRoles.Admin) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isEditPermissible = canEdit(UserRoles.Editor);
+
+console.log(isEditPermissible);
