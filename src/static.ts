51ba008848519e100abd94c61b2fbba6706cc6aa
Tuku <tukuwebian@gmail.com>
2026-05-05 21:23:56 +0300

    static learn

---
+++ src/static.ts
@@ -0,0 +1,16 @@
+class Counter {
+  static count = 0;
+  static increment() {
+    return (Counter.count = Counter.count + 1);
+  }
+  static decrement() {
+    return (Counter.count = Counter.count - 1);
+  }
+}
+
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
+
+console.log(Counter.decrement());
